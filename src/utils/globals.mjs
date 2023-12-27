304a1c5f2f7850b54eadc550ea0c50f5de5b0908
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2023-12-27 20:01:45 +0100

    video recording of canvas

--- src/utils/globals.mjs
+++ src/utils/globals.mjs
@@ -1,8 +1,20 @@
 //==================================
 // Const
 //==================================
-
-export const Globals = Object.freeze({
+export const Screen = Object.freeze({
   yt_width: 1920,
   yt_height: 1080,
 });
+
+
+//==================================
+// Functions
+//==================================
+export function getRgb( rgb ) {
+  const match = rgb.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
+  return match ? { red: parseInt(match[1]), green: parseInt(match[2]), blue: parseInt(match[3]) } : {};
+}
+
+export function randInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
--- src/views/CanvasDrawing.vue
+++ src/views/CanvasDrawing.vue
@@ -1,5 +1,8 @@
 <template>
-  <Canvas ref="canvas_ref" :width="WIDTH" :height="HEIGHT" />
+  <canvas ref="canvas_ref" :width="WIDTH" :height="HEIGHT" />
+  <video :video_src="video_src" autoplay controls />
+  <Btn v-if="is_recording" class="rec-btn" :def="false" @click="stopRecording">Stop video</Btn>
+  <Btn v-else class="rec-btn" :def="false" @click="startRecording">Start video</Btn>
 </template>
 
 <script setup>
@@ -12,37 +15,62 @@ import {
   onMounted,
   onUnmounted,
 } from "vue";
-import { Globals } from "../utils/globals.mjs";
+import { Screen, randInt } from "../utils/globals.mjs";
 
-import Canvas from "../components/Canvas.vue";
+import Btn from "../components/Btn.vue";
 
 //==================================
 // Const
 //==================================
-const WIDTH = Globals.yt_width / 2;
-const HEIGHT = Globals.yt_height / 2;
+const WIDTH      = Screen.yt_width / 2;
+const HEIGHT     = Screen.yt_height / 2;
 const TOT_POINTS = 30;
-const W_SEGMENT = WIDTH / TOT_POINTS;
-// const HUE_INCREMENT = 30;
-// const H_SEGMENT  = HEIGHT / TOT_POINTS;
+const W_SEGMENT  = WIDTH / TOT_POINTS;
+const FPS = 20;
 
-const ctx        = ref( undefined );
-const canvas_ref = ref( undefined );
-const pause      = ref( false );
-const points     = ref( [] );
+const ctx          = ref( undefined );
+const canvas_ref   = ref( undefined );
+const is_pause     = ref( false );
+const points       = ref( [] );
+const is_recording = ref( false );
 
 let animation_frame = undefined;
+let media_recorder  = undefined;
+let video_stream    = undefined;
+let frames          = [];
+const video_src     = ref( undefined );
 
 //==================================
 // Functions
 //==================================
-function initLoop() {
-  animation_frame = window.requestAnimationFrame(initLoop);
-  update();
+function startRecording() {
+  is_recording.value = true;
+  media_recorder.start();
 }
 
-function update() {
-  setCanvas();
+function stopRecording() {
+  is_recording.value = false;
+  media_recorder.stop();
+}
+
+function initLoop() {
+  getVideoFrame();
+  clearCanvas();
+  drawPoints();
+  animation_frame = window.requestAnimationFrame(initLoop);
+}
+
+function getVideoFrame() {
+  if ( video_stream ) { 
+    video_stream.getVideoTracks()[0].requestFrame();
+   }
+}
+
+function clearCanvas() {
+  ctx.value.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
+function drawPoints() {
   for (let i = 0; i < points.value.length; i++) {
     const p = points.value[i];
     const next_p = points.value[i+1] || undefined;
@@ -77,18 +105,6 @@ function setColor() {
   return `rgb(${ rgb.red },${ rgb.green },${ rgb.blue })`;
 }
 
-// function getRgb( rgb ) {
-//   const match = rgb.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
-//   return match ? {
-//     red: parseInt(match[1]),
-//     green: parseInt(match[2]),
-//     blue: parseInt(match[3])
-//   } : {};
-// }
-
-function setCanvas() {
-  ctx.value.clearRect(0, 0, WIDTH, HEIGHT);
-}
 
 function bounceToBoundaries( p ) {
   let has_bounced = false;
@@ -120,16 +136,6 @@ function drawCircle({ x, y, radius, color }) {
   ctx.value.fill();
 }
 
-function onkeydown(e) {
-  if (e.code == "Space") {
-    pause.value = !pause.value;
-  }
-}
-
-function randInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function initPoints() {
   for( let i = 1; i <= TOT_POINTS; i++ ) {
     const radius = i * 0.1618;
@@ -146,10 +152,27 @@ function initPoints() {
   }
 }
 
+function initRecord() {
+  video_stream = canvas_ref.value.captureStream( FPS );
+  media_recorder = new MediaRecorder( video_stream );
+  media_recorder.ondataavailable = (e) => frames.push( e.data );
+  media_recorder.onstop = () => {
+    const blob = new Blob(frames, { 'type' : 'video/mp4' });
+    video_src.value = URL.createObjectURL(blob);
+    frames = [];
+  };
+}
+
+function onkeydown(e) {
+  if (e.code == "Space") {
+    is_pause.value = !is_pause.value;
+  }
+}
+
 //==================================
 // Watch
 //==================================
-watch(pause, (newVal) => {
+watch(is_pause, (newVal) => {
   newVal ? window.cancelAnimationFrame(animation_frame) : initLoop();
 });
 
@@ -158,8 +181,9 @@ watch(pause, (newVal) => {
 //==================================
 onMounted(() => {
   window.addEventListener("keydown", onkeydown);
-  ctx.value = canvas_ref.value.ctx;
+  ctx.value = canvas_ref.value.getContext('2d');
   initPoints();
+  initRecord();
   initLoop();
 });
 
@@ -167,7 +191,29 @@ onUnmounted(() => {
   window.cancelAnimationFrame(animation_frame);
   window.removeEventListener("keydown", onkeydown);
 });
+
+
 </script>
 
 <style lang="scss" scoped>
+.rec-btn {
+  position: absolute;
+  bottom: 12px;
+  right: 12px;
+}
+video {
+  position: absolute;
+  top: 12px;
+  right: 12px;
+  width: 220px;
+  height: auto;
+  border: 1px solid #222;
+}
+canvas {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 2px solid #222;
+  }
 </style>
